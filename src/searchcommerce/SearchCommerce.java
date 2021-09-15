094f453b6fbac8cc987134c5aa5805be7b0ed1bb
Thiago Quevedo <thiagoquevedodossantos@gmail.com>
2021-09-15 14:58:54 -0300

    Criada base do projeto

---
+++ src/searchcommerce/SearchCommerce.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchcommerce;
+
+/**
+ *
+ * @author Thiago Quevedo
+ */
+public class SearchCommerce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
