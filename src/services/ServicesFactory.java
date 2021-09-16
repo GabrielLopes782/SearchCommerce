5127c482fbc74197e9bcdb6f50f01eaecbe7c29c
Thiago Quevedo <thiagoquevedodossantos@gmail.com>
2021-09-16 15:18:51 -0300

    Inserindo os dados de conexão com o banco de dados

--- src/services/ServicesFactory.java
+++ src/services/ServicesFactory.java
@@ -3,22 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package searchcommerce;
-
-
+package services;
 
 /**
  *
  * @author Thiago Quevedo
  */
-public class SearchCommerce {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-    }
+public class ServicesFactory {
     
 }
